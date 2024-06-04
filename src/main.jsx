d2fe6ee7495aec2c2f99b6855b3b02f36ef943bb
Kh.Eliash <eliashebrahim@gmail.com>
2024-06-04 23:55:49 +0600

    arrenge data in tabular formate for admin

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
--- src/pages/dashboard/admin/AdManage.jsx
+++ src/pages/dashboard/admin/AdManage.jsx
@@ -1,10 +1,77 @@
- 
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "../../../others/Axios/useAxios";
+
 const AdManage = () => {
-    return (
+  const axios = useAxios();
+
+  const { data: allAdReq = [], refetch } = useQuery({
+    queryKey: ["email"],
+    queryFn: async () => {
+      const res = await axios.get("/sellerAdds");
+      return res.data;
+    },
+  });
+  console.log(allAdReq);
+
+  const handleToggle = (e, id) => {
+    console.log(e.target.checked);
+    console.log(id);
+  };
+
+  return (
+    <div>
+      <h2 className="text-center my-4 font-bold ">
+        Choose Image for Banner slider
+      </h2>
+      <hr />
+      <div>
         <div>
-            ad
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Favorite Color</th>
+                  <th>Accept/Deny</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {allAdReq.map((item, index) => (
+                  <tr key={item._id}>
+                    <th>{index + 1}</th>
+                    <td>
+                      <div>
+                        <div className="font-bold">{item.name}</div>
+                      </div>
+                    </td>
+                    <td>{item.email}</td>
+                    <td>
+                      <img
+                        src={item.image}
+                        alt=""
+                        className="w-24 h-12 rounded-md"
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        className="toggle"
+                        onChange={(e) => handleToggle(e, item._id)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AdManage;
